a5823a37abd891dc6db1ae8f5973b0889a384f65
Demeugaliyev <m.demeugaliev@gmail.com>
2018-10-05 09:05:40 +0600

    root

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2018_10_04_205832) do
     t.string "name"
     t.string "sdate"
     t.string "expdate"
-    t.string "ident"
+    t.integer "id", null: false
     t.string "img"
     t.string "geo"
     t.string "ksk"
